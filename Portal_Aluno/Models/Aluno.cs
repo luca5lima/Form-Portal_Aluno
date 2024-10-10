f87ba27c6012aba4beb4999e3dfa1160a9f61481
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-10 12:20:48 -0300

    Criação de Validações para o preenchimento dos campos

--- Portal_Aluno/Models/Aluno.cs
+++ Portal_Aluno/Models/Aluno.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,32 @@ namespace Portal_Aluno.Models
 {
     public class Aluno
     {
-        public Aluno(string nome, int idade, string curso)
-        {
-            Nome = nome;
-            Idade = idade;
-            Curso = curso;
-        }
 
         public string Nome { get; private set; }
         public int Idade { get; private set; }
         public string Curso { get; private set; }
+
+        public Aluno(string nome, int idade, string curso)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new Expression("Nome é obrigatorio!");
+            }
+
+            if (string.IsNullOrEmpty(idade))
+            {
+                throw new Expression("Idade é obrigatorio!");
+            }
+
+            if (string.IsNullOrEmpty(curso))
+            {
+                throw new Expression("Curso é obrigatorio!");
+            }
+
+            Nome = nome;
+            Idade = int.Parse(idade);
+            Curso = curso;
+        }
+
     }
 }
