2d9fe11ee8ade4a44e7228006f2a0a044f7a81a5
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-10 00:00:22 -0300

    Adicionar um armazenamento de dados - Criar um modelo de Aluno

---
+++ Portal_Aluno/Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal_Aluno.Models
+{
+    public class Aluno
+    {
+        public Aluno(string nome, int idade, string curso)
+        {
+            Nome = nome;
+            Idade = idade;
+            Curso = curso;
+        }
+
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+        public string Curso { get; private set; }
+    }
+}
