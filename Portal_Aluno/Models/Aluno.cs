5956c497990b418c5e50bf53a2e39c88d4894986
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-10-10 20:05:36 -0300

    Correções na Class Aluno

--- Portal_Aluno/Models/Aluno.cs
+++ Portal_Aluno/Models/Aluno.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,32 +8,31 @@ namespace Portal_Aluno.Models
 {
     public class Aluno
     {
-
-        public string Nome { get; private set; }
-        public int Idade { get; private set; }
-        public string Curso { get; private set; }
-
-        public Aluno(string nome, int idade, string curso)
+        public Aluno(string nome, string idade, string curso)
         {
             if (string.IsNullOrEmpty(nome))
             {
-                throw new Expression("Nome é obrigatorio!");
+                throw new Exception("Nome é obrigatorio!");
             }
 
             if (string.IsNullOrEmpty(idade))
             {
-                throw new Expression("Idade é obrigatorio!");
+                throw new Exception("Idade é obrigatorio!");
             }
 
             if (string.IsNullOrEmpty(curso))
             {
-                throw new Expression("Curso é obrigatorio!");
+                throw new Exception("Curso é obrigatorio!");
             }
 
             Nome = nome;
             Idade = int.Parse(idade);
             Curso = curso;
         }
+
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+        public string Curso { get; private set; }
 
     }
 }
